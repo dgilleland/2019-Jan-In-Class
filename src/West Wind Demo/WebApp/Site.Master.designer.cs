9782a217340160c8dc5872f85a8dbf17d68dc6d1
Dan Gilleland <DGilleland@nait.ca>
2019-02-08 14:02:20 -0700

    Added nav menu items

--- src/West Wind Demo/WebApp/Site.Master.designer.cs
+++ src/West Wind Demo/WebApp/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace WebApp {
     public partial class SiteMaster {
         
         /// <summary>
+        /// StaffOnly control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl StaffOnly;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>

23d538762de0d978ab7d10b59e2b46b47a178274
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-02-26 09:43:33 -0700

    Quick sandbox introduction to Web Forms

---
+++ src/Sandbox Demo/WebApp/About.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApp.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information. Enter your full name and click the button.</p>
+
+<%--    Tips:
+    - [Ctrl] + [k] , [Ctrl] + [c] will comment out your selected text
+    - [Ctrl] + [k] , [Ctrl] + [u] will un-comment  your selected text
+    - [Ctrl] + [k] , [Ctrl] + [d] will format the code in your current file--%>
+    
+    <%--I am using this label for general messages--%>
+    <asp:Label ID="MessageLabel" runat="server" />
+
+    <%--The ID attribute will generate a field name in the code-behind class
+    that allows me to programmatically interact with the asp.net server control.--%>
+    <asp:TextBox ID="FullName" runat="server" />
+    
+    <%--A LinkButton will "look like" a link (render as an <a></a>), but "work like" a button--%>
+    <asp:LinkButton ID="GetInitials" runat="server"
+        CssClass="btn btn-primary" Text="Get Your Initials"
+        OnClick="GetInitials_Click" />
+    <%--The OnClick that I specify here associates the click event of the button with a
+    method handler in the code-behind of the page.--%>
+
+    <asp:Label ID="Initials" runat="server" />
+    <asp:LinkButton ID="ReverseName" runat="server"
+        CssClass="btn btn-default" Text="Spell it backwards"
+        Visible="false" OnClick="ReverseName_Click" />
+</asp:Content>
---
+++ src/Sandbox Demo/WebApp/About.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Is a "stage" of processing the page where you get to set up
+            // aspects of the page's content before any of the events of 
+            // your page's controls are processed.
+
+            if(!IsPostBack) // !IsPostBack means a GET request
+            {
+                MessageLabel.Text = "Tell us about yourself! You can trust us!";
+                // Label    .String
+            }
+            else
+            {
+                MessageLabel.Text = string.Empty; // clear the contents
+            }
+        }
+
+        protected void GetInitials_Click(object sender, EventArgs e)
+        {
+            // This method is what gets called when the <asp:LinkButton ID="GetInitials" ...
+            // is clicked.
+            // We can say that this method "handles" the click event of that button/link.
+            
+            // Take the user's input, and create an array of strings (use space as delimiter)
+            var nameParts = FullName.Text.Split(' ');
+            // Construct the initials by taking the first character of each word part.
+            string result = "";
+            foreach (var part in nameParts)
+                if (!string.IsNullOrWhiteSpace(part))
+                    result += $"{part[0]}.";
+            // Place the result in the label on the form
+            Initials.Text = result;
+
+            if (string.IsNullOrEmpty(result))
+            {
+                MessageLabel.Text = "I can't get initials from empty text...";
+                MessageLabel.CssClass = "bg-info";
+            }
+            else
+                ReverseName.Visible = true; // Show the button
+        }
+
+        protected void ReverseName_Click(object sender, EventArgs e)
+        {
+            string reversed = string.Join("", FullName.Text.ToArray().Reverse());
+            MessageLabel.Text = $"Your name reversed is <b>{reversed}</b>";
+        }
+    }
+}

c7be0c6430441cbe513f48be7994afad5ae3ed7f
Dan Gilleland <DGilleland@nait.ca>
2019-02-26 16:03:59 -0700

    Bulk processing of ListView data

--- src/Databound Controls/WebApp/Demos/CourseEditor.aspx
+++ src/Databound Controls/WebApp/Demos/CourseEditor.aspx
@@ -10,10 +10,32 @@
     <asp:TextBox ID="StartsOn" runat="server" CssClass="form-content" TextMode="Date" />
 
     <asp:ListView ID="AssignmentsList" runat="server"
-         InsertItemPosition="FirstItem" ItemType="BackEnd.BLL.Commands.WeightedItem">
-
+         InsertItemPosition="FirstItem" ItemType="BackEnd.BLL.Commands.WeightedItem"
+         OnItemInserting="AssignmentsList_ItemInserting">
+        <InsertItemTemplate>
+            <div>
+                <asp:TextBox ID="ItemWeight" runat="server" Text="<%# BindItem.Weight %>" TextMode="Number" placeholder="% Weight" />
+                <asp:TextBox ID="ItemTitle" runat="server" Text="<%# BindItem.AssignmentName %>" placeholder="Assignment Name" />
+                <asp:LinkButton ID="AddItem" runat="server" CommandName="Insert" Text="Add Assignment"
+                     CssClass="btn btn-default" />
+            </div>
+        </InsertItemTemplate>
+        <ItemTemplate>
+            <div>
+                <asp:Label ID="Weight" runat="server" Text="<%# Item.Weight %>" /> % &ndash;
+                <asp:Label ID="AssignmentTitle" runat="server" Text="<%# Item.AssignmentName %>" />
+            </div>
+        </ItemTemplate>
+        <LayoutTemplate>
+            <%--The ListView looks for a server-run control with an id of 
+            itemPlaceholder
+            as the element to replace with the contents of a template
+            for each item.--%>
+            <div id="itemPlaceholder" runat="server" />
+        </LayoutTemplate>
     </asp:ListView>
 
-    <asp:LinkButton ID="AddCourse" runat="server" CssClass="btn btn-primary" Text="Add Course" />
+    <asp:LinkButton ID="AddCourse" runat="server" CssClass="btn btn-primary" Text="Add Course"
+         OnClick="AddCourse_Click"/>
 
 </asp:Content>
--- src/Databound Controls/WebApp/Demos/CourseEditor.aspx.cs
+++ src/Databound Controls/WebApp/Demos/CourseEditor.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BackEnd.BLL;
+using BackEnd.BLL.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,75 @@ namespace WebApp.Demos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                PopulateAssignments(null);
+            }
+        }
 
+        // This method is populating the ListView with WeightedItem objects
+        // that have been entered by the user
+        private void PopulateAssignments(WeightedItem item)
+        {
+            RetrieveAssignments();
+            if (item != null)
+                Assignments.Add(item);
+            AssignmentsList.DataSource = Assignments;
+            AssignmentsList.DataBind();
+        }
+
+        private void RetrieveAssignments()
+        {
+            // Loop through all the items in the ListView, and add them back to
+            // our List<WeightedItem> field.
+            foreach(ListViewDataItem item in AssignmentsList.Items)
+            {
+                // Use the item to find the control that has the data we want
+                var nameLabel = item.FindControl("AssignmentTitle") as Label;
+                var weightLabel = item.FindControl("Weight") as Label;
+
+                if(nameLabel != null && weightLabel != null)
+                {
+                    var existing = new WeightedItem
+                    {
+                        AssignmentName = nameLabel.Text,
+                        Weight = int.Parse(weightLabel.Text)
+                    };
+                    Assignments.Add(existing);
+                }
+            }
+        }
+
+        // A local list of objects that we rebuild each time the PopulateAssignments()
+        // method get called.
+        private List<WeightedItem> Assignments = new List<WeightedItem>();
+
+        // This method handles the list view's OnItemInserting command.
+        // That command gets triggered by the linkbutton with the CommandName="Insert"
+        protected void AssignmentsList_ItemInserting(object sender, ListViewInsertEventArgs e)
+        {
+            // Grab the values that were inserted.
+            string name = e.Values[nameof(WeightedItem.AssignmentName)].ToString();
+            int weight = Convert.ToInt32(e.Values[nameof(WeightedItem.Weight)]);
+            var newItem = new WeightedItem { AssignmentName = name, Weight = weight };
+
+            // bind the listview with the added information
+            PopulateAssignments(newItem);
+        }
+
+        // Final processing for this page to add courses to our system.
+        protected void AddCourse_Click(object sender, EventArgs e)
+        {
+            // 1) Extract the data from the form
+            DateTime start;
+            DateTime.TryParse(StartsOn.Text, out start);
+            var courseInfo = new CourseOffering(CourseName.Text, start);
+
+            RetrieveAssignments();
+
+            // 2) Send the data to the BLL for processing
+            var controller = new StudentGradesController();
+            controller.CreateCourse(courseInfo, 7, Assignments);
         }
     }
 }

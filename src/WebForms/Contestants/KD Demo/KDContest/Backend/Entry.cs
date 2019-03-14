1794465858bac5a089d7b1887dff6ce67ac5eabf
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-03-14 08:47:24 -0600

    Classes

---
+++ src/WebForms/Contestants/KD Demo/KDContest/Backend/Entry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KDContest.Backend
+{
+    public class Entry
+    {
+        public string EntryCode { get; set; }
+        public string Email { get; set; }
+
+        // Greedy constructor
+        public Entry(string code, string email)
+        {
+            EntryCode = code;
+            Email = email;
+        }
+    }
+
+    public class Entrant
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public int Age { get; set; }
+
+        public Entrant(string firstName, string lastName, string email, string city, string prov, string postalCode, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            City = city;
+            Province = prov;
+            PostalCode = postalCode;
+            Age = age;
+        }
+    }
+}

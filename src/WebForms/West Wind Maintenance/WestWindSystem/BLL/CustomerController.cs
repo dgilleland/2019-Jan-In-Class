8bf837bbb326c1394000da9ccb1385fe3fe7a9a6
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-03-29 12:03:31 -0600

    Controllers calling stored procedures

--- src/WebForms/West Wind Maintenance/WestWindSystem/BLL/CustomerController.cs
+++ src/WebForms/West Wind Maintenance/WestWindSystem/BLL/CustomerController.cs
@@ -18,12 +18,29 @@ namespace WestWindSystem.BLL
 
         public List<Customer> GetCustomersByCompanyName(string partialCompanyName)
         {
+            // This method is allowing me to get a filtered result
+            // of "rows" of customers from the database by using
+            // a stored procedure that gets this short list.
             using (var context = new WestWindContext())
             {
+                // My SQL string is a call to the store procedure.
+                // In Sql Server, a parameter is denoted by the @ symbol.
+                // Parameters are numbered rather than named, and the
+                // numbering starts at zero.
                 string sql = "EXEC Customers_GetByPartialCompanyName @0";
-                DbRawSqlQuery<Customer> result = context.Database.SqlQuery<Customer>(sql, partialCompanyName);
+                // We call our context class' .Database object to run
+                // the SQL Query with the expected return type.
+                DbRawSqlQuery<Customer> result = 
+                    context.Database.SqlQuery<Customer>(sql, partialCompanyName);
+                //                           \row type/      \     @0         /
+                // The order of arguments sent in relates to the SQL parameter's
+                // position.
 
-                return result.ToList();
+                // When debugging, it can helpful to use another variable
+                // to hold the result of the .ToList()
+                var finalResult = result.ToList();
+
+                return finalResult;
             }
         }
 
--- src/WebForms/West Wind Maintenance/WestWindSystem/BLL/OrderController.cs
+++ src/WebForms/West Wind Maintenance/WestWindSystem/BLL/OrderController.cs
@@ -14,5 +14,15 @@ namespace WestWindSystem.BLL
                 return context.Orders.ToList();
             }
         }
+
+        public List<Order> ListOrders(string byCustomerId)
+        {
+            using (var context = new WestWindContext())
+            {
+                string sql = "EXEC Orders_GetByCustomer @0";
+                var result = context.Database.SqlQuery<Order>(sql, byCustomerId);
+                return result.ToList();
+            }
+        }
     }
 }

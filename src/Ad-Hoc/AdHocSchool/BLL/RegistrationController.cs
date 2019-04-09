98efe378771959391c57eb70251fb093a176ad6d
Dan Gilleland <DGilleland@nait.ca>
2019-04-09 14:24:22 -0600

    Added comments to my code + small bugfix

--- src/Ad-Hoc/AdHocSchool/BLL/RegistrationController.cs
+++ src/Ad-Hoc/AdHocSchool/BLL/RegistrationController.cs
@@ -4,8 +4,17 @@ using System.Threading.Tasks;
 
 namespace AdHocSchool.BLL
 {
+    /// <summary>
+    /// This controller is used to register students in courses.
+    /// </summary>
     public class RegistrationController
     {
+        /// <summary>
+        /// Registers a set of students for first-term courses (course id is "xxxx1#x"
+        /// where the 1 indicates first year and the # is a first-term course when
+        /// the number is less than 5).
+        /// </summary>
+        /// <param name="cohort"></param>
         public void RegisterFirstTermStudents(StudentRegistration cohort)
         {
             // Courses.Where(x => x.CourseId[4] == '1' && x.CourseId[5] < '5')
--- src/Ad-Hoc/AdHocSchool/DataModels/StudentRegistration.cs
+++ src/Ad-Hoc/AdHocSchool/DataModels/StudentRegistration.cs
@@ -6,13 +6,36 @@ using System.Threading.Tasks;
 
 namespace AdHocSchool.DataModels
 {
+    /// <summary>
+    /// Represents a set of students to be registered for 
+    /// a specific term intake in a specific year.
+    /// </summary>
     public class StudentRegistration
     {
-        public enum Term { SEP, JAN, MAY}
-        public Term Month { get; set; }
-        public int Year { get; set; }
-        public string Semester => $"{Month.ToString()[0]}{Year}";
+        /// <summary>
+        /// A enumerated data type of the months in which school terms start
+        /// </summary>
+        public enum Term { SEP, JAN, MAY } // an enumeration is a primitive set of named constants
 
+        /// <summary>
+        /// The school term in which this specific cohort of students is being registered.
+        /// </summary>
+        public Term Month { get; set; }
+
+        /// <summary>
+        /// The calendar year corresponding with the <see cref="Month"/> of the school term.
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Is a calculation of the school term and year to produce a value that matches the
+        /// expectations of the school database.
+        /// </summary>
+        public string Semester => $"{Year}{Month.ToString()[0]}";
+
+        /// <summary>
+        /// A list of student Ids that presumably exist in the database.
+        /// </summary>
         public ICollection<int> StudentIds { get; set; }
             = new HashSet<int>();
     }
